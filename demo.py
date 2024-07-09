098ff53bb802faf0b6671744da96df44d547b0d5
Taj <mdtajhassan19@gmail.com>
2024-07-09 14:23:45 +0530

    demo file updated

--- demo.py
+++ demo.py
@@ -1,15 +1,3 @@
 
-import requests
-
-url = 'https://reqres.in/api/users'
-body = {
-    "name": "morpheus",
-    "job": "leader"
-}
-header = 'application/json'
-status_code = 201
-
-response = requests.post(url, json=body, verify=False)
-print(f"{response.status_code} Is response code.")
-assert status_code == response.status_code
+print("Hello World this is my first demo fyle.....")
 
